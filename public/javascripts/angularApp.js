2269e0e15526e1533502ebfe1f9bccf489b9bc8c
Joe Macbook Pro <neeaagh@gmail.com>
2015-11-05 15:27:56 -0500

    created angular factory for authentication

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -113,3 +113,47 @@ app.factory('posts', ['$http', function($http) {
   };
 	return o; // is this necessary
 }]);
+
+app.factory('auth', ['$http', '$window', function($http, $window) {
+  var auth = {};
+
+  auth.saveToken = function(token) {
+    $window.localStorage['flappy-news-token'] = token;
+  };
+
+  auth.getToken = function() {
+    return $window.localStorage['flappy-news-token'];
+  };
+
+  auth.isLoggedIn = function() {
+    var token = auth.getToken();
+
+    if(token) {
+      var payload = JSON.parse($window.atob(token(token.split('.')[1])));
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  auth.currentUser = function() {
+    if(auth.isLoggedIn()) {
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.username;
+    }
+  };
+
+  auth.register = function(user) {
+    return $http.post('/register', user).success(function(data) {
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logOut = function() {
+    $window.localStorage.removeItem('flappy-news-token');
+  };
+
+  return auth;
+}]);
